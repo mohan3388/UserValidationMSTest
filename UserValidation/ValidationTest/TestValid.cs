2a2648aee473fa83bcb4396dc47e192d2df7bc70
mohan <sahumohan338@gmail.com>
2022-08-22 03:52:31 -0700

    [mohan]add: Parameterized Multiple Email Entry

--- UserValidation/ValidationTest/TestValid.cs
+++ UserValidation/ValidationTest/TestValid.cs
@@ -26,9 +26,19 @@ namespace ValidationTest
         public void User_Valid_email()
         {
             User valid = new User();
-            bool actual = valid.EmailValidation("abc.xyz@bl.co.in");
-
+            bool actual = valid.EmailValidation("abc@yahoo.com");
             Assert.IsTrue(actual);
+            bool actual2 = valid.EmailValidation("abc-100@yahoo.com");
+            Assert.IsTrue(actual2);
+            bool actual3 = valid.EmailValidation("abc.100@yahoo.com");
+            Assert.IsTrue(actual3);
+            bool actual4 = valid.EmailValidation("abc111@abc.com");
+            Assert.IsTrue(actual4);
+            bool actual5 = valid.EmailValidation("abc-100@abc.net");
+            Assert.IsTrue(actual5);
+            bool actual6 = valid.EmailValidation("abc.100@abc.com.au");
+            Assert.IsTrue(actual6);
+
         }
         [Test]
         public void User_Valid_Mobile()
