9e51af34ca8011de8b3b6622c8006fdc391146a7
mohan <sahumohan338@gmail.com>
2022-08-22 05:33:56 -0700

    [mohan]add:

--- UserValidation/UserValidation/User.cs
+++ UserValidation/UserValidation/User.cs
@@ -13,28 +13,14 @@ namespace UserValidation
         public static string EMAIL_REGEX_PATTERN = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
         public static string MOBILE_REGEX_PATTERN = "^[91]{2}[' ']{1}[7898625487]{10}$";
         public static string PASS_REGEX_PATTERN = "^[A-Za-z]{8,}";
-        
-        public bool FNameValidation(string name)
-        {
-            return Regex.IsMatch(name, REGEX_PATTERN);
-        }
-        public bool LNameValidation(string lname)
-        {
-            return Regex.IsMatch(lname, REGEX_PATTERN);
-        }
-        public bool EmailValidation(string email)
-        {
-            return Regex.IsMatch(email, EMAIL_REGEX_PATTERN);
-        }
-        public bool MobileValidation(string mobile)
-        {
-            return Regex.IsMatch(mobile, MOBILE_REGEX_PATTERN);
-        }
-        public bool PassValidation(string pass)
-        {
-            return Regex.IsMatch(pass, PASS_REGEX_PATTERN);
-        }
+
        
 
+        public bool FNameValidation(string Firstname) => Regex.IsMatch(Firstname, REGEX_PATTERN) ? true : throw new UserException(UserException.ExceptionType.INVALID_FIRSTNAME,"First name is invalid");
+        public bool LNameValidation(string Lastname) => Regex.IsMatch(Lastname, REGEX_PATTERN) ? true : throw new UserException(UserException.ExceptionType.INVALID_LASTNAME, "Last name is invalid");
+
+        public bool EmailValidation(string Email) => Regex.IsMatch(Email, EMAIL_REGEX_PATTERN) ? true : throw new UserException(UserException.ExceptionType.INVALID_EMAIL, "Email is invalid");
+        public bool MobileValidation(string Mobile) => Regex.IsMatch(Mobile, MOBILE_REGEX_PATTERN) ? true : throw new UserException(UserException.ExceptionType.INVALID_MOBILENUMBER, "Mobile number is invalid");
+        public bool PassValidation(string Password) => Regex.IsMatch(Password, PASS_REGEX_PATTERN) ? true : throw new UserException(UserException.ExceptionType.INVALID_PASSWORD, "Password is invalid");
     }
 }
